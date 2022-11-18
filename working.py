145987bf93c5108eed1b42cbf47543ab5fb3480d
Matt Cottrell <mattcottrell93@hotmail.co.uk>
2022-11-18 18:28:19 +0000

    5th: Using isinstance to seperate behaviour. Also moved pay method out of class, into Employee classes. Also made into abstract classin Employee parent class

--- working.py
+++ working.py
@@ -2,6 +2,7 @@
 Very advanced Employee management system.
 """
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import List
@@ -21,12 +22,17 @@ class Role(Enum):
 
 
 @dataclass
-class Employee:
+class Employee(ABC):
     """Basic representation of an employee at the company."""
 
     name: str
     role: Role
     vacation_days: int = 25
+
+    @abstractmethod
+    def pay(self) -> None:
+        """Method to call when paying an employee"""
+        pass
 
     def take_a_holiday(self, payout: bool) -> None:
         """Let the employee take a single holiday, or pay out 5 holidays."""
@@ -59,12 +65,21 @@ class HourlyEmployee(Employee):
     hourly_rate_dollars: float = 50
     hours_worked: int = 10
 
+    def pay(self) -> None:
+        print(
+            f"Paying employee {self.name} a hourly rate of \
+                ${self.hourly_rate} for {self.amount} hours."
+        )
+
 
 @dataclass
 class SalariedEmployee(Employee):
     """Employee that's paid based on a fixed monthly salary."""
 
     monthly_salary: float = 5000
+
+    def pay(self) -> None:
+        print("Paying employee {self.name} a monthly salary of ${self.monthly_salary}.")
 
 
 class Company:
@@ -81,18 +96,6 @@ class Company:
         """Find all employees with a particular role"""
         return [employee for employee in self.employees if employee.role is role]
 
-    def pay_employee(self, employee: Employee) -> None:
-        """Pay an employee."""
-        if isinstance(employee, SalariedEmployee):
-            print(
-                f"Paying employee {employee.name} a monthly salary of ${employee.monthly_salary}."
-            )
-        elif isinstance(employee, HourlyEmployee):
-            print(
-                f"Paying employee {employee.name} a hourly rate of \
-                ${employee.hourly_rate} for {employee.amount} hours."
-            )
-
 
 def main() -> None:
     """Main function."""
@@ -106,7 +109,8 @@ def main() -> None:
     print(company.find_employees(role=Role.VICEPRESIDENT))
     print(company.find_employees(role=Role.MANAGER))
     print(company.find_employees(role=Role.INTERN))
-    company.pay_employee(company.employees[0])
+
+    company.employees[0].pay()
     company.employees[0].take_a_holiday(False)
 
 
